65ef286df047ba4623307dc8186d12a822096704
RaymondZziwa <raymondzian@gmail.com>
2024-08-14 15:22:05 +0300

    preset

---
+++ reader-mgt/prisma/migrations/20240814083919_latest_preset/migration.sql
@@ -0,0 +1,18 @@
+/*
+  Warnings:
+
+  - The primary key for the `article` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - You are about to alter the column `id` on the `article` table. The data in that column could be lost. The data in that column will be cast from `VarChar(191)` to `Int`.
+  - The primary key for the `reader` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - You are about to alter the column `id` on the `reader` table. The data in that column could be lost. The data in that column will be cast from `VarChar(191)` to `Int`.
+
+*/
+-- AlterTable
+ALTER TABLE `article` DROP PRIMARY KEY,
+    MODIFY `id` INTEGER NOT NULL AUTO_INCREMENT,
+    ADD PRIMARY KEY (`id`);
+
+-- AlterTable
+ALTER TABLE `reader` DROP PRIMARY KEY,
+    MODIFY `id` INTEGER NOT NULL AUTO_INCREMENT,
+    ADD PRIMARY KEY (`id`);
