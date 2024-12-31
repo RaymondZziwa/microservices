6030bde9ee3b568019f13872500b271a03077899
RaymondZziwa <raymondzian@gmail.com>
2024-12-31 20:38:32 +0300

    preset

--- api-gateway/src/nats-client/nats.module.ts
+++ api-gateway/src/nats-client/nats.module.ts
@@ -8,7 +8,7 @@ import { ClientsModule, Transport } from '@nestjs/microservices';
         name: 'NATS_SERVICE',
         transport: Transport.NATS,
         options: {
-          servers: ['nats://nats-server:4222'],
+          servers: ['nats://nats:4222'],
           //servers: ['nats://localhost:4222'],
         },
       },
@@ -22,7 +22,7 @@ import { ClientsModule, Transport } from '@nestjs/microservices';
         transport: Transport.NATS,
         options: {
           //servers: ['nats://localhost:4222'],
-          servers: ['nats://nats-server:4222'],
+          servers: ['nats://nats:4222'],
         },
       },
     ]),
